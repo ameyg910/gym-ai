948a12d7a4f28b74b975520f7862b78377cdb4c1
Amey Gupta <ameygupta@Ameys-MacBook-Pro.local>
2025-11-23 11:38:13 +0530

    Generate program functions coded

--- app/generate-program/page.tsx
+++ app/generate-program/page.tsx
@@ -1,4 +1,108 @@
+"use client"; 
+import { useState } from "react";
+import { useRouter } from "next/router"
+import { useUser } from "@clerk/nextjs";
+import { useRef } from "react";
+import { useEffect } from "react";
+import { vapi } from "@/lib/vapi";
+import { TelemetryPlugin } from "next/dist/build/webpack/plugins/telemetry-plugin/telemetry-plugin";
 const GenerateProgramPage = () => {
+  const [callActive, setCallActive] = useState(false);
+  const [connecting, setConnecting] = useState(false);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [messages, setMessages] = useState([]); 
+  const [callEnded, setCallEnded] = useState(false);
+
+  const {user} = useUser()
+  const router = useRouter()
+
+  const messagesContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if(messagesContainerRef.current) {
+       messagesContainerRef.current.scrollTop = messagesContainerRef.current.scrollHeight;
+    }
+  }, [messages])
+
+  useEffect(() => {
+    if(callEnded) {
+      const redirectTimer = setTimeout(() => {
+         router.push('/profile')
+      }, 1500);
+      return () => clearTimeout(redirectTimer);
+    }
+  }, [callEnded, router])
+  useEffect(() => {
+    const handleCallStart = () => {
+      console.log("Call started");
+      setConnecting(false);
+      setCallActive(true);
+      setCallEnded(false);
+    }
+    const handleCallEnd = () => {
+      console.log("Call ended");
+      setCallActive(false);
+      setConnecting(false);
+      setIsSpeaking(false);
+      setCallEnded(true);
+    }
+    const handleSpeechStart = () => {
+      console.log("AI started speaking"); 
+      setIsSpeaking(true);
+    }
+    const handleSpeechEnd = () => {
+      console.log("AI finished speaking");
+      setIsSpeaking(false);
+    }
+
+    const handleMessage = (message: any) => {}
+    const handleError = (error: any) => {
+      console.error("Vapi error:", error);
+      setConnecting(false);
+      setCallActive(false); 
+
+    }
+     vapi.on("call-start", handleCallStart)
+         .on("call-end", handleCallEnd)
+         .on("speech-start", handleSpeechStart)
+         .on("speech-end", handleSpeechEnd)
+         .on("message", handleMessage)
+         .on("error", handleError);
+         return () => {
+    vapi.off("call-start", handleCallStart)
+        .off("call-end", handleCallEnd)
+        .off("speech-start", handleSpeechStart)
+        .off("speech-end", handleSpeechEnd)
+        .off("message", handleMessage)
+        .off("error", handleError);
+  }   
+         }, [])
+
+         const toggleCall = async () => {
+    if(callActive) {
+      vapi.stop();
+    } 
+      else{
+        try{
+          setConnecting(true); 
+          setMessages([]); 
+          setCallEnded(false);
+
+          const fullName = user?.firstName 
+          ? `${user.firstName}  ${user.lastName || ""}`.trim()
+          : "There";
+          await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
+            variableValues: {
+              full_name: fullName
+            }
+          })
+ 
+        }catch(error){
+          console.log("Failed to start Vapi call:", error);
+          setConnecting(false);
+        }
+      }
+         }
   return (
     <div>
       GenerateProgramPage
